cc4c35e648931be16f101c84dfd088bc394e6009
Adam Wojtycki <adamwojtycki@gmail.com>
2023-10-30 12:50:32 +0100

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @type { PrettierConfig } */
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 160,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
