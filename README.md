a9eebac1ed2690668ca97b87a333d29dea4b8a0f
aadamw <adamwojtycki@gmail.com>
2023-11-04 13:12:59 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <p align="center">A collection of React UI Snippets.</p>
 
+<center>React UI Snippets is a curated collection of React components crafted with Tailwind CSS + Framer Motion components</center>
+
 <p align="center">made by <a target="__blank" href="https://twitter.com/__aadamw">@__aadamw</a></p>
--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,10 @@ export default function Home() {
     <main className="container mx-auto px-2 gap-4 py-10 min-h-screen flex flex-col justify-between">
       <div>
         <div className="flex justify-center flex-col items-center gap-2">
-          <h1 className="font-medium text-center text-3xl">React UI Snippets</h1>
+          <h1 className="font-semibold text-center text-3xl">React UI Snippets</h1>
+          <p className="text-xs text-white/60 text-center">
+            React UI Snippets is a curated collection of React components crafted with Tailwind CSS + Framer Motion components
+          </p>
           <CreatedByLink />
         </div>
         <div className="grid mt-10 sm:grid-cols-2 gap-4 xl:grid-cols-3  xl:gap-6">
